3b6fdd9cc9c3d6f7617d6459bdc45dcc58392ba2
Azher-Bashir <azherbashir07@gmail.com>
2025-03-15 23:43:55 +0500

    Working on PYTHON and making it more better

--- python/filename.py
+++ python/filename.py
@@ -1,4 +1,12 @@
 print("Hello World!")
+print(24, 45)
+
+name = 889
+print("Enter your name: ")
+input(name)
+# print(name)
+
+str(name)
 
 # printting my first line of code in vscode
 # hope that I will have nice eexperience with this language
