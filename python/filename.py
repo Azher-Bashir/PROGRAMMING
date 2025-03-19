1777decd30043f721f85c6fd3fca827dff64eb6e
Azher-Bashir <azherbashir07@gmail.com>
2025-03-19 20:16:43 +0500

    workinng on python

--- python/filename.py
+++ python/filename.py
@@ -1,12 +1,10 @@
-print("Hello World!")
-print(24, 45)
-
-name = 889
-print("Enter your name: ")
-input(name)
-# print(name)
-
-str(name)
-
-# printting my first line of code in vscode
-# hope that I will have nice eexperience with this language
+num_1 = int(input("Enter the first number for the simple functions: "))
+num_2 = int(input("Enter the second number for the simple functions: "))
+sum = num_1 + num_2
+pro = num_1*num_2
+div = num_1 / num_2
+mod = num_1 % num_2
+print ("Sum of the two numbers is: ",sum)
+print ("Product of the two numbers is: ", pro)
+print ("Quotient of the two numbers after division is: ",div)
+print ("Remainder of the two numbers after division is: ",mod)
