b29ab30581cc044e7b090514ceecff602dad5d90
Azher-Bashir <azherbashir07@gmail.com>
2025-04-01 11:37:37 +0500

    Creating a game using python

--- python/tkinter_vscode/snake.py
+++ python/tkinter_vscode/snake.py
@@ -9,9 +9,24 @@ WINDOW_WIDTH = COLS * TILE_SIZE
 WINDOW_HEIGHT = ROWS * TILE_SIZE
 
 #gmae window
-windows = tkinter.tk()
+window = tkinter.Tk()
 
-windows.title("Snake game")
-windows_resizable = (False, False)
+window.title("Snake")
+window.resizable(False, False)
+Canvas = tkinter.Canvas(window, width = WINDOW_WIDTH, height = WINDOW_HEIGHT, bg = "black", highlightthickness = 0, border = 0)
+Canvas.pack()
+window.update()
 
-windows.mainloop()
+#center the window
+window_width = window.winfo_width()
+window_height = window.winfo_height()
+screen_width = window.winfo_screenwidth()
+screen_height = window.winfo_screenheight()
+
+window_x = int((screen_width/2) - (window_width/2))
+window_y = int((screen_height/2) - (window_height/2))
+
+#format ((w)*(h)+(x)+(y))
+window.geometry(f"{window_width}x{window_height}+{window_x}+{window_y}")
+
+window.mainloop()
